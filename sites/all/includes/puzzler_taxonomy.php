3df12726dab1c126ddb796116d3f9921ad5bc0ef
Peter Needle <pneedle@gmail.com>
2019-04-28 13:23:24 -0400

    added reference to wikipedia.org

--- sites/all/includes/puzzler_taxonomy.php
+++ sites/all/includes/puzzler_taxonomy.php
@@ -1,22 +1,25 @@
 <?php
 
-// get the current path of the taxonomy term
-$url = current_path();
+  // get the current path of the taxonomy term
+  $url = current_path();
 
-// strip out everything except the taxonomy term id (tid)
-$tid = substr($url, strrpos($url, '/') + 1);
-//echo "taxonomy term id : " . $tid . "<br />";
+  // strip out everything except the taxonomy term id (tid)
+  $tid = substr($url, strrpos($url, '/') + 1);
+  //echo "taxonomy term id : " . $tid . "<br />";
 
-// load the taxonomy term that corresponds to the tid
-$term = taxonomy_term_load($tid);
+  // load the taxonomy term that corresponds to the tid
+  $term = taxonomy_term_load($tid);
 
-// extract the term name
-$name = $term->name;
-//echo "taxonomy name: " . $name . "<br />";
+  // extract the term name
+  $name = $term->name;
+  //echo "taxonomy name: " . $name . "<br />";
 
-$wikipedia_into = "[puzzler:wikipedia-matching-intro:" . $name . "]";
-$wikipedia_imagehtml = "[puzzler:wikipedia-matching-image-html:" . $name . "]";
+  // Puzzler shortcode
+  $wikipedia_into = "[puzzler:wikipedia-matching-intro:" . $name . "]";
+  $wikipedia_imagehtml = "[puzzler:wikipedia-matching-image-html:" . $name . "]";
 
-echo token_replace($wikipedia_into);
-echo token_replace($wikipedia_imagehtml);
+  echo token_replace($wikipedia_into);
+  echo token_replace($wikipedia_imagehtml);
+  echo "<hr />";
+  echo "Reference: <a href='https://www.wikipedia.org'>wikipedia.org</a>";
 ?>
